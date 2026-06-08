4ec7de7338ea349b1af3799742e565aa98709169
molqzone <toiperus@sohu.com>
2026-06-08 22:16:58 +0800

    refactor(control): move teleop remote into upstream controller

--- rmgo_ws/src/rmgo_core/include/rmgo_core/remote.hpp
+++ rmgo_ws/src/rmgo_core/include/rmgo_core/remote.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-#include <memory>
+#include <concepts>
 
 #include <controller_interface/controller_interface_base.hpp>
 #include <rclcpp/time.hpp>
@@ -16,20 +16,18 @@ struct RemoteCommand
   bool valid = false;
 };
 
-class RemoteBase {
-public:
-  virtual ~RemoteBase() = default;
-
-  virtual controller_interface::CallbackReturn configure(
-    const std::shared_ptr<rclcpp_lifecycle::LifecycleNode> & node) = 0;
-
-  virtual controller_interface::CallbackReturn activate() = 0;
-
-  virtual controller_interface::CallbackReturn deactivate() = 0;
-
-  virtual void set_enabled(bool enabled) = 0;
-
-  virtual RemoteCommand calculate(const rclcpp::Time & time) = 0;
-};
+template<typename Device>
+concept RemoteDevice =
+  std::default_initializable<Device> &&
+  requires(
+    Device device,
+    const std::shared_ptr<rclcpp_lifecycle::LifecycleNode> & node,
+    const rclcpp::Time & time)
+  {
+    { device.configure(node) } -> std::same_as<controller_interface::CallbackReturn>;
+    { device.activate() } -> std::same_as<controller_interface::CallbackReturn>;
+    { device.deactivate() } -> std::same_as<controller_interface::CallbackReturn>;
+    { device.calculate(time) } -> std::same_as<RemoteCommand>;
+  };
 
 }  // namespace rmgo_core
