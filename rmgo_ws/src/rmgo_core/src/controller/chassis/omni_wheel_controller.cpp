82703519cd1cad2af5e489f0f2212f30823b4f93
molqzone <toiperus@sohu.com>
2026-06-10 09:04:12 +0800

    refactor(control): keep internal headers in source tree

--- rmgo_ws/src/rmgo_core/src/controller/chassis/omni_wheel_controller.cpp
+++ rmgo_ws/src/rmgo_core/src/controller/chassis/omni_wheel_controller.cpp
@@ -13,7 +13,7 @@
 #include <rclcpp_lifecycle/state.hpp>
 
 #include "rmgo_core/omni_wheel_controller_config.hpp"
-#include "rmgo_core/pid/pid_calculator.hpp"
+#include "../pid/pid_calculator.hpp"
 
 namespace rmgo_core::controller::chassis {
 
